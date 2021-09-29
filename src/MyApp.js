c9163440799c4aac55bae8d1233926111cde0a89
Bruno da Silva <brunocarreiro@gmail.com>
2021-09-28 22:45:03 -0700

    Updating files

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import Table from './Table';
 import Form from './Form';
 
@@ -22,6 +23,36 @@ function MyApp() {
       }
   ]);
 
+  useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+     });
+  }, [] );
+
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:5000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+  }
+
+  async function makePostCall(person){
+    try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -30,7 +61,10 @@ function MyApp() {
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    makePostCall(person).then( result => {
+      if (result && result.status === 200)
+         setCharacters([...characters, person] );
+      }); 
   }
 
   return (
